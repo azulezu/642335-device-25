4a1b98b8757e6bfc9fb46b86ba49f695da4b9a91
Anna <anna.zueva.mail@inbox.ru>
2019-06-16 20:13:43 +0300

    Модальные окна
    1. Сделала стилизацию
    2. Обработка кнопок открытия и закрытия на javascript
    3. Другой вариант visually-hidden - как на лекции (должен корректно работать с полями ввода)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,58 @@ drop.addEventListener("click", function(evt) {
   submenu.classList.toggle("visually-hidden");
 });
 
+// ------------------- модальные окна ----------------------
+
+var feedback = document.querySelector(".feedback");
+var feedback_close = feedback.querySelector(".modal-close");
+var feedback_link = document.querySelector(".feedback-link");
+
+// показать форму
+feedback_link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  feedback.classList.add("modal-show");
+});
+
+//  скрыть по крестику
+feedback_close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  feedback.classList.remove("modal-show");
+});
+
+//  скрыть по эскейпу
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (feedback.classList.contains("modal-show")) {
+        feedback.classList.remove("modal-show");
+      }
+    }
+  });
+
+// ----
+
+var map = document.querySelector(".map");
+var map_close = map.querySelector(".modal-close");
+var map_link = document.querySelector(".map-link");
+
+// показать форму
+map_link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  map.classList.add("modal-show");
+});
+
+//  скрыть по крестику
+map_close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  map.classList.remove("modal-show");
+});
+
+//  скрыть по эскейпу
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (map.classList.contains("modal-show")) {
+        map.classList.remove("modal-show");
+      }
+    }
+  });
